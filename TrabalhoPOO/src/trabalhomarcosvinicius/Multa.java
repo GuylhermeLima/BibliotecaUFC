aa9a939fc01fa9ad1ca2356c94fb53e383d3881e
Lívia <142369124+LiviSantos@users.noreply.github.com>
2023-12-02 16:04:20 -0300

    Update Multa.java

--- TrabalhoPOO/src/trabalhomarcosvinicius/Multa.java
+++ TrabalhoPOO/src/trabalhomarcosvinicius/Multa.java
@@ -1,25 +1,15 @@
 package trabalhomarcosvinicius;
 
-import java.util.*;
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class Multa {
-    
-    private Aluno aluno;
-    private Servidor servidor;
-    private Livro livro;
+	
     private BigDecimal valorMulta;
-    private LocalDateTime dataReserva;
-    private LocalDateTime dataRetiradaEmprestimo; //ADICIONAR NO DIAGRAMA
-    private LocalDateTime dataVencimento;    
-    private boolean emprestimoAtivo;
+    private boolean prazoAtrasado;
 
         
     //COMPARA A DATA DE DE RETIRADA DO EMPRESTIMO COM A DATA DE VENCIMENTO E CALCULA A MULTA BASEADO NO ATRAZO
-    public void calculoMulta() {
+    public void calcularMulta() {
         
     }
     
@@ -35,72 +25,23 @@ public class Multa {
 
     
     //GETTERS
-    public Aluno getAluno() {
-        return this.aluno;
-    }
-
-    public Servidor getServidor() {
-        return this.servidor;
-    }
-
-    public Livro getLivro() {
-        return this.livro;
-    }
-
     public BigDecimal getValorMulta() {
         return this.valorMulta;
     }
-
-    public LocalDateTime getDataReserva() {
-        return this.dataReserva;
-    }
-
-    public LocalDateTime getDataRetiradaEmprestimo() {
-        return this.dataRetiradaEmprestimo;
-    }
-
-    public LocalDateTime getDataVencimento() {
-        return this.dataVencimento;
-    }
-
-    public boolean getEmprestimoAtivo() {
-        return this.emprestimoAtivo;
-    }
+    
+	public boolean isPrazoAtrasado() {
+		return prazoAtrasado;
+	}
 
 
     //SETTERS
-
-    public void setAluno(Aluno aluno) {
-        this.aluno = aluno;
-    }
-
-    public void setServidor(Servidor servidor) {
-        this.servidor = servidor;
-    }
-
-    public void setLivro(Livro livro) {
-        this.livro = livro;
-    }
-
     public void setValorMulta(BigDecimal valorMulta) {
-        this.valorMulta = valorMulta;
+    	if(valorMulta != null) {
+    		this.valorMulta = valorMulta;
+        }
     }
 
-    public void setDataReserva(LocalDateTime dataReserva) {
-        this.dataReserva = dataReserva;
-    }
-
-    public void setDataRetiradaEmprestimo(LocalDateTime dataRetiradaEmprestimo) {
-        this.dataRetiradaEmprestimo = dataRetiradaEmprestimo;
-    }
-
-    public void setDataVencimento(LocalDateTime dataVencimento) {
-        this.dataVencimento = dataVencimento;
-    }
-
-    public void setEmprestimoAtivo(boolean emprestimoAtivo) {
-        this.emprestimoAtivo = emprestimoAtivo;
-    }
-    
-    
+	public void setPrazoAtrasado(boolean prazoAtrasado) {
+		this.prazoAtrasado = prazoAtrasado;
+	}
 }
