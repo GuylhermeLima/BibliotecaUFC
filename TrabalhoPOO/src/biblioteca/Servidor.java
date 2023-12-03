628b6a3fee6fcee9a6111f06d33c41fd34a1e0a2
Lívia <142369124+LiviSantos@users.noreply.github.com>
2023-12-03 16:15:29 -0300

    Update Servidor.java

--- TrabalhoPOO/src/biblioteca/Servidor.java
+++ TrabalhoPOO/src/biblioteca/Servidor.java
@@ -3,33 +3,58 @@ package biblioteca;
 public class Servidor extends Pessoa{
 
     private String dataEfetivacao;
-        
-    
-    // AQUI OS SERVIDORES PODEM APLICAR UMA CADASTRO NO SISTEMA
+	    
+	private List<Servidor> servidores;
 
-    public void cadastarServidor() {
-        
-    }
-    public void removerServidor() {
-        
-    }
-    public void alterarDadosServidor() {
-        
-    }
-    
-    
-    //GETTERS
+	public Servidor() {
+	        servidores = new ArrayList<>();
+	}
 
-    public String getDataEfetivacao() {
-        return this.dataEfetivacao;
-    }
+	    // AQUI OS SERVIDORES PODEM APLICAR UMA CADASTRO NO SISTEMA
+	    
+	public void cadastrarServidor(Servidor servidor) {
+	        servidores.add(servidor);
+	}
 
-    
-    //SETTERS
+	public void removerServidor(long id) {
+	        servidores.removeIf(servidor -> servidor.getId() == id);
+	}
 
-    public void setDataEfetivacao(String dataEfetivacao) {
-        this.dataEfetivacao = dataEfetivacao;
-    }
-    
-    
+
+	public void alterarDados(long id, String nome, String senha, String email, String cpf, String telefone, String dataNascimento, String dataEfetivacao) {
+	        for (Servidor servidor : servidores) {
+	            if (servidor.getId() == id) {
+	                servidor.setNome(nome);
+	                servidor.setSenha(senha);
+	                servidor.setEmail(email);
+	                servidor.setCpf(cpf);
+	                servidor.setTelefone(telefone);
+	                servidor.setDataNascimento(dataNascimento);
+	                servidor.setDataEfetivacao(dataEfetivacao);
+	                break;
+	            }
+	        }
+	}
+
+	    // METODOS PARA LOGIN E LOGOFF NO SISTEMA
+	    
+	public Servidor loginServidor(long id, String senha) {
+	        for (Servidor servidor : servidores) {
+	            if (servidor.getId() == id && servidor.getSenha().equals(senha)) {
+	                return servidor;
+	            }
+	        }
+	        return null;
+	}
+	    //GETTERS
+
+	public String getDataEfetivacao() {
+	        return this.dataEfetivacao;
+	}
+
+	    //SETTERS
+	    
+	public void setDataEfetivacao(String dataEfetivacao) {
+	        this.dataEfetivacao = dataEfetivacao;
+	}
 }
